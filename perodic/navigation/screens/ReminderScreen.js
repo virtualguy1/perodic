a13ef82eaeb2ef6601f5246a4bd0bb0b4b47a35f
Abhinav <asrivastav_mca21@thapar.edu>
2022-04-30 22:55:29 +0530

    reminder screen

--- perodic/navigation/screens/ReminderScreen.js
+++ perodic/navigation/screens/ReminderScreen.js
@@ -1,12 +1,111 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import Task from './Task';
 
 export default function ReminderScreen({ navigation }) {
+    const [task, setTask] = useState();
+    const [taskItems, setTaskItems] = useState([]);
+  
+    const handleAddTask = () => {
+      Keyboard.dismiss();
+      setTaskItems([...taskItems, task])
+      setTask(null);
+    }
+  
+    const completeTask = (index) => {
+      let itemsCopy = [...taskItems];
+      itemsCopy.splice(index, 1);
+      setTaskItems(itemsCopy)
+    }
+  
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Reminder Screen</Text>
+      <View style={styles.container}>
+        {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+        <ScrollView
+          contentContainerStyle={{
+            flexGrow: 1
+          }}
+          keyboardShouldPersistTaps='handled'
+        >
+  
+        {/* Today's Tasks */}
+        <View style={styles.tasksWrapper}>
+          <View style={styles.items}>
+            {/* This is where the tasks will go! */}
+            {
+              taskItems.map((item, index) => {
+                return (
+                  <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
+                    <Task text={item} /> 
+                  </TouchableOpacity>
+                )
+              })
+            }
+          </View>
         </View>
+          
+        </ScrollView>
+  
+        {/* Write a task */}
+        {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+        <KeyboardAvoidingView 
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.writeTaskWrapper}
+        >
+          <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
+          <TouchableOpacity onPress={() => handleAddTask()}>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+        
+      </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#E8EAED',
+    },
+    tasksWrapper: {
+      paddingTop: 20,
+      paddingHorizontal: 20,
+    },
+    sectionTitle: {
+      fontSize: 24,
+      fontWeight: 'bold'
+    },
+    items: {
+      marginTop: 10,
+    },
+    writeTaskWrapper: {
+      position: 'absolute',
+      bottom: 20,
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center'
+    },
+    input: {
+      paddingVertical: 10,
+      paddingHorizontal: 10,
+      backgroundColor: '#FFF',
+      borderRadius: 60,
+      borderColor: 'tomato',
+      borderWidth: 1,
+      width: 260,
+    },
+    addWrapper: {
+      width: 50,
+      height: 50,
+      backgroundColor: '#FFF',
+      borderRadius: 50,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderColor: 'tomato',
+      borderWidth: 1,
+    },
+    addText: {},
+  });
